e9f02390868615e2bae72d098a242f4e0c19dcb7
abdul <abdul.tanveer@gmail.com>
2022-04-25 10:49:23 +0530

    initial commit

---
+++ springharman/src/springharman/model/Test.java
@@ -0,0 +1,20 @@
+package springharman.model;
+
+import org.springframework.beans.factory.BeanFactory;  
+import org.springframework.beans.factory.xml.XmlBeanFactory;  
+import org.springframework.core.io.ClassPathResource;  
+import org.springframework.core.io.Resource;  
+
+public class Test {  
+	public static void main(String[] args) {  
+		Resource resource=new ClassPathResource("applicationContext.xml");
+		//applicationContext.xml is my configuration file
+		BeanFactory factory=new XmlBeanFactory(resource);  
+
+		Student student = (Student)factory.getBean("studentbean");  
+		//im not instantiating a Student object -- new Student()
+		//instead the spring is instantiating it from me and injecting it
+		
+		student.displayInfo();  
+	}  
+}  
